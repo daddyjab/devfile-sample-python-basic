0769f21c15e71f9b6a53b773c43e7ef5a919fceb
Jeff Brown <dadjab@att.net>
2023-04-21 17:38:20 -0500

    Added a demo endpoint -- flipping the order of the routes

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import os
 
 app = Flask(__name__)
 
+app.route('/demo')
+def demo_the_site():
+    return "This is a demo of the site"
+
 @app.route('/')
 def hello():
     return "Hello World!"
-
-@app.route('/demo')
-def demo_the_site():
-    return "This is a demo of the site"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
