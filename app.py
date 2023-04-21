b5085243322d3e94fa76fd9e8a51c4d993a36c2c
Jeff Brown <dadjab@att.net>
2023-04-21 18:17:54 -0500

    Turned on debug mode

--- app.py
+++ app.py
@@ -3,16 +3,17 @@ import os
 
 app = Flask(__name__)
 
-app.route('/demo')
-def demo_the_site():
-    return "This is a demo of the site"
-
 @app.route('/')
+@app.route('/hello')
 def hello():
     return "Hello World!"
+
+@app.route('/hello2')
+def hello2():
+    return "This is a different 'hello'!"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
     port = int(port)
 
-    app.run(port=port,host='0.0.0.0')
+    app.run(port=port,host='0.0.0.0',debug=True)
